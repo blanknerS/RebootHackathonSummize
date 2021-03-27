f7664318be52725384d73124bc8adcac79e96b16
blanknerS <81494057+blanknerS@users.noreply.github.com>
2021-03-27 19:15:40 -0400

    Add files via upload
    These files are the code we wrote, this will not run as google credentials are not present, nor the packages. This is just the pure code.

---
+++ getVideoClips.py
@@ -0,0 +1,49 @@
+import toText
+import moviepy.editor as mp
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+from datetime import datetime as dt
+import datetime
+import os
+import zipfiles
+
+def get_clips(path, words):
+    date = dt.now().strftime("%Y~%m~%d@%H-%M")
+    videoFile = mp.VideoFileClip(path)
+    desktopPath = os.path.expanduser("~/Desktop")
+    audioPath = os.path.expanduser("~/Desktop/myAudio.flac")
+    toText.upload_to_bucket("myAudio", audioPath, "sttproject")
+    times_and_words = toText.transcribe_gcs_with_word_time_offsets("gs://sttproject/myAudio", words)
+
+    os.remove(f"{desktopPath}/myAudio.flac")
+    timestamps = times_and_words[1::2]
+    words = times_and_words[0::2]
+    convertedTimeStamps = []
+    clip = 0
+    for time, name in zip(timestamps, words):
+        if time - 10.0 < 0:
+            start = 0
+        else:
+            start = time - 10
+
+        if time + 10 > videoFile.duration:
+            end = videoFile.duration
+        else:
+            end = time + 10
+        if not os.path.exists(f"{desktopPath}/subClips"):
+            os.makedirs(f"{desktopPath}/subClips")
+        if not os.path.exists(f"{desktopPath}/subClips/{date}"):
+            os.makedirs(f"{desktopPath}/subClips/{date}")
+
+        convertedTimeStamp = str(datetime.timedelta(seconds=time))[:7]
+        convertedTimeStamp = convertedTimeStamp.replace(":", "-")
+        convertedTimeStamps.append(name)
+        convertedTimeStamps.append(convertedTimeStamp)
+
+        smallClip = ffmpeg_extract_subclip(path, start, end, targetname=f"{desktopPath}/subClips/{date}/{name}@{convertedTimeStamp}.mp4")
+        clip = clip + 1
+    zipfiles.zippIt()
+    return convertedTimeStamps
+
+# print(convertedTimeStamps)
+# 
+# get_clips("/Users/blakeankner/Desktop/CLIP1.mp4", "apple, banana, peach")
---
+++ toAudio.py
@@ -0,0 +1,12 @@
+import moviepy.editor as mp
+import subprocess
+import os
+
+def make_audio(videoPath):
+    desktopPath = os.path.expanduser("~/Desktop")
+    clip = mp.VideoFileClip(videoPath)
+    clip.audio.write_audiofile(f"{desktopPath}/myAudio.wav")
+    subprocess.call(['avconv', '-i', f'{desktopPath}/myAudio.wav', '-y', '-ar', '48000', '-ac', '1', f'{desktopPath}/myAudio.flac'])
+    os.remove(f"{desktopPath}/myAudio.wav")
+
+# make_audio("/Users/blakeankner/Desktop/CLIP1.mp4")
